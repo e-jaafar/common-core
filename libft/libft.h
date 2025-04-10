13687bf96a20d2b74c6092380eee9429e376f509
elmrabet.jaafar <elmrabet.jaafar@gmail.com>
2025-04-10 12:13:30 +0200

    maj functions

---
+++ libft/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jael-m-r <jael-m-r@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/10 11:37:22 by jael-m-r          #+#    #+#             */
+/*   Updated: 2025/04/10 12:13:02 by jael-m-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str);
+char	*ft_strcpy(char *dest, char *src);
+int	ft_atoi(char *str);
+
+
+
+
+
+#endif
